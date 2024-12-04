ab65e51d59db1b8a3ccdfdac6120d970ede5accc
GNGscr <de.brand808@gmail.com>
2024-12-04 21:59:16 +0200

    hero text align mobile 3

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -162,7 +162,7 @@ export default function Hero({ media }) {
                             <motion.div 
                               { ...hoverStatesAnimations }
                               style={{ borderRadius: "50%" }}
-                              className="relative p-2 border-2 border-[#000]"
+                              className="absolute p-2 border-2 border-[#000]"
                               >
                                 <svg style={{rotate: "90deg"}} width="24" height="24" stroke="#000" strokeWidth="1px"  xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/></svg>
                             </motion.div>
