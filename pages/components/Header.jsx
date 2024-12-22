bac1eb793a6fdc8d837647990e07df231e166cf0
GNGscr <de.brand808@gmail.com>
2024-12-22 13:00:09 +0200

    header color change 1

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -102,8 +102,8 @@ const ZoopEffect = ({ children, href, media }) => {
   );
   const mobileColor = useTransform(
     scrollYProgress,
-    [0, 0.18, 0.435, 0.865, 1],
-    ["#000", "#000", "#ddd", "rgb(252 165 165)", "rgb(252 165 165)"],
+    [0, 0.2525, 0.4525, 0.65, 0.865, 1],
+    ["#000", "#000", "#ddd", "000", "rgb(252 165 165)", "rgb(252 165 165)"],
     { ease: (t) => Math.round(t) }
   );
   useEffect(() => {
