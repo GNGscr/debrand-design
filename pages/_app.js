faecde87c8b1f7c082c6961317913c44913c6436
GNGscr <de.brand808@gmail.com>
2025-07-11 13:47:45 +0300

    hook google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,11 @@ import Layout from '../pages/components/Layout';
 import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
+  const NEXT_PUBLIC_GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
   return (
     <Layout>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-3JQ1HLK15T"
+        src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -15,7 +16,7 @@ export default function App({ Component, pageProps }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-3JQ1HLK15T');
+          gtag('config', ${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID});
         `}
       </Script>
       <Component {...pageProps} />
