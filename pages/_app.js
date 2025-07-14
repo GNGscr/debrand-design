6ffaa7f7465d5cf5f072af7a176be104ae5fd2f2
GNGscr <de.brand808@gmail.com>
2025-07-14 20:26:00 +0300

    fix script

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,24 @@ export default function App({ Component, pageProps }) {
   const NEXT_PUBLIC_GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
   return (
     <Layout>
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+      {NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
+        <>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-          gtag('config', ${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID});
-        `}
-      </Script>
+              gtag('config', '${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+            `}
+          </Script>
+        </>
+      )}
       <Component {...pageProps} />
     </Layout>
   );
