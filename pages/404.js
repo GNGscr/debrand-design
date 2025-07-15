6bb21da500f0a6cc7274cc6ece08505abd076bd5
GNGscr <de.brand808@gmail.com>
2025-07-15 15:39:52 +0300

    updating 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,68 +1,59 @@
 import Link from "next/link";
 import Head from "next/head";
 import SectionAnimation from "./components/SectionAnimation";
-import media from "./hooks/useMediaQuery";
 import { motion } from "framer-motion";
 
 export default function Custom404() {
+  const sentence = "The page you're looking for doesn't exist.";
+  const letters = sentence.split("");
   return (
     <>
       <Head>
         <title>404 – Page Not Found</title>
       </Head>
       <div className="flex flex-col items-center justify-center min-h-screen bg-[#121212] text-white px-4 text-center">
-        <h1 className="text-6xl font-bold mb-4">
-          {/* 404 */}
-          <div
-          className="w-full flex align-center justify-center text-[3rem] mb-[5px]"
-          style={{ opacity: 0.7 }}>
-            {
-              '404'.split('').map((letter, i) => {
-                return (
-                  <motion.div
-                    key={i}
-                    initial={{ opacity: 0, y: "120%", skewX: 15 }}
-                    whileInView={{ opacity: 1, y: 0, skewX: 0 }}
-                    transition={{ duration: 0.35, type: "spring", delay: i * 0.03, }}
-                  >
-                    {letter}
-                  </motion.div>
-                )
-              })
-            }
-        </div>
-        </h1>
+        <h1 className="text-6xl font-bold mb-4">404</h1>
         <div className="404-dolphin-animation">
-          <SectionAnimation
-              menuIsActive={true}
-              media={media}
-            />
+          <SectionAnimation menuIsActive={true} media={"mobile"} />
         </div>
-        <p className="text-2xl mb-6">
-          {`The page you're looking for doesn't exist.`}
-        </p>
-        <Link
-          href="/"
-          className="text-2xl text-blue-300 hover:text-blue-200"
-        >
-          <motion.span
+        <div className="text-2xl mb-6">
+          <div className="w-full flex align-center justify-center text-[1.5rem] mb-[5px]">
+            {letters.map((char, i) => {
+              return (
+                <motion.div
+                  key={i}
+                  initial={{ opacity: 0, y: "75%", skewX: 15 }}
+                  whileInView={{ opacity: 1, y: 0, skewX: 0 }}
+                  transition={{
+                    duration: 0.25,
+                    type: "spring",
+                    delay: i * 0.025,
+                  }}
+                >
+                  {char === " " ? "\u00A0" : char}
+                </motion.div>
+              );
+            })}
+          </div>
+        </div>
+
+        <motion.span
           initial={{ scale: 1 }}
           animate={{ scale: [1, 1.25, 1, 1.25, 1] }}
           transition={{
-            delay: 0.5,
+            delay: 1.35,
             duration: 0.75,
             ease: "easeInOut",
             times: [0, 0.25, 0.5, 0.75, 1],
           }}
         >
           <Link
-            href="/"
+            href="/about"
             className="text-2xl text-blue-300 hover:text-blue-200"
           >
             ← Back to Home
           </Link>
         </motion.span>
-        </Link>
       </div>
     </>
   );
