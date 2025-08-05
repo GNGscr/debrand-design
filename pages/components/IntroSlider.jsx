3806f8ba78b03e0cf9d2366b4800bee2f91ee616
GNGscr <de.brand808@gmail.com>
2025-08-05 12:01:31 +0300

    prevent scroll on intro

--- pages/components/IntroSlider.jsx
+++ pages/components/IntroSlider.jsx
@@ -4,38 +4,75 @@ import gsap from "gsap";
 import Hero from '../components/Hero';
 import { motion } from "framer-motion";
 
-export default function IntroSlider({ media }) {
+export default function IntroSlider({ media, setOverlow }) {
   const comp = useRef(null);
   
   useEffect(() => {
     if (typeof window !== 'undefined') {
       let ctx = gsap.context(() => {
         const t1 = gsap.timeline();
-        t1.from('#intro-slider', { xPercent: '-100', duration: .75, delay: .15 })
-        .from(['#title-1', '#title-2', '#title-3'], { display: 'flex', opacity: 0, y: '+=30', stagger: .2 })
-        .to(['#title-1', '#title-2', '#title-3'], { opacity: 0, y: '-=30', delay: .015, stagger: .25, })
-        .to('#intro-slider', { xPercent: '-100', duration: 1.15 })
-        .to(['#title-1', '#title-2', '#title-3'], { display: 'none' })
-        .from('#inner-hero-title',{ opacity: 0, duration: .5, yPercent: '+=100', rotate: 5 }, "<-0.75")
+        t1.from("#intro-slider", {
+          xPercent: "-100",
+          duration: 0.75,
+          delay: 0.15,
+        })
+          .from(["#title-1", "#title-2", "#title-3"], {
+            display: "flex",
+            opacity: 0,
+            y: "+=30",
+            stagger: 0.2,
+          })
+          .to(["#title-1", "#title-2", "#title-3"], {
+            opacity: 0,
+            y: "-=30",
+            delay: 0.015,
+            stagger: 0.25,
+          })
+          .to("#intro-slider", { xPercent: "-100", duration: 1.15 })
+          .to(["#title-1", "#title-2", "#title-3"], { display: "none" })
+          .from(
+            "#inner-hero-title",
+            { opacity: 0, duration: 0.5, yPercent: "+=100", rotate: 5 },
+            "<-0.75"
+          );
+        // Set the overflow to hidden after the animation is done
+        t1.eventCallback("onComplete", handleTransitionEnd);
       }, comp);
       return () => ctx.revert();
     }
   }, []);
+  
+  const handleTransitionEnd = () => { 
+    if (typeof window !== 'undefined') {
+      setOverlow();
+    }
+  }
 
   return (
     <div className="relative" ref={comp}>
-      <motion.div 
+      <motion.div
         id="intro-slider"
-        initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.1}} style={{opacity: 0}}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.1 }}
+        style={{ opacity: 0 }}
         className={`h-[100%] min-h-screen p-4 md:p-20 bg-gray-50 absolute top-0 left-0
-          z-10 w-full flex flex-col gap-4 md:gap-10 tracking-tight justify-start`}>
-          <h1 id="title-1" className="text-4xl md:text-9xl font-bold">Software Engineer</h1>
-          <h1 id="title-2" className="text-4xl md:text-9xl font-bold">Creative Developer</h1>
-          <h1 id="title-3" className="text-4xl md:text-9xl font-bold">Full Stack 70 ~ 30</h1>
+          z-10 w-full flex flex-col gap-4 md:gap-10 tracking-tight justify-start`}
+      >
+        <h1 id="title-1" className="text-4xl md:text-9xl font-bold">
+          Software Engineer
+        </h1>
+        <h1 id="title-2" className="text-4xl md:text-9xl font-bold">
+          Creative Developer
+        </h1>
+        <h1 id="title-3" className="text-4xl md:text-9xl font-bold">
+          Full Stack 70 ~ 30
+        </h1>
       </motion.div>
-      <div 
-        style={{clipPath: "polygon(0% 0, 100% 0, 100% 100%, 0 100%)"}} 
-        className={`h-screen flex bg-red-300 justify-center place-items-center`}>
+      <div
+        style={{ clipPath: "polygon(0% 0, 100% 0, 100% 100%, 0 100%)" }}
+        className={`h-screen flex bg-red-300 justify-center place-items-center`}
+      >
         <Hero media={media} />
       </div>
     </div>
--- pages/components/MainWrapper.jsx
+++ pages/components/MainWrapper.jsx
@@ -1,27 +1,35 @@
 "use client";
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
 import IntroSlider from "./IntroSlider";
 import SectionTransition from "./SectionTransition";
 import Header from "./Header";
-import ProjectsSection from './ProjectsSection';
+import ProjectsSection from "./ProjectsSection";
 import useMediaQuery from "../hooks/useMediaQuery";
 
 export default function MainWrapper() {
-    const ref = useRef(null);
-    const container = useRef();
-    const media = useMediaQuery(ref);
+  const ref = useRef(null);
+  const container = useRef();
+  const media = useMediaQuery(ref);
+  const [isOverflowHidden, setIsOverflowHidden] = useState(true);
 
-    return (
-        <div ref={ref} className="main-wrapper">
-            <IntroSlider media={media} />
-            {
-                media === 'desktop'
-                ? <div className="top-screen h-screen left-0 relative">
-                    <SectionTransition media={media} />
-                  </div>
-                : <ProjectsSection media={media} container={container} />
-            }
-            <Header media={media} />
+  return (
+    <div
+      ref={ref}
+      className="main-wrapper"
+      style={isOverflowHidden ? { overflowY: "hidden", height: "100vh" } : {}}
+    >
+      <IntroSlider
+        media={media}
+        setOverlow={() => setIsOverflowHidden(false)}
+      />
+      {media === "desktop" ? (
+        <div className="top-screen h-screen left-0 relative">
+          <SectionTransition media={media} />
         </div>
-    )
+      ) : (
+        <ProjectsSection media={media} container={container} />
+      )}
+      <Header media={media} />
+    </div>
+  );
 }
